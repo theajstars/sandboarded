0b1fb84f06873f186c047b482732aeb2222e4d20
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2022-03-04 10:03:37 +0100

    Goals section

--- src/Components/GoalStats.jsx
+++ src/Components/GoalStats.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import { UserGoals } from "../Assets/JSON/Goals.json";
 export default function GoalStats() {
   return (
-    <div className="goals-section">
+    <div className="goals-section flex-column">
       <span
         className="suprema card-section-header flex-row"
         style={{ alignItems: "center" }}
@@ -12,8 +12,30 @@ export default function GoalStats() {
           <i className="fas fa-plus"></i>
         </span>
       </span>
-      <br />
-      <div className="flex-row goals-outline"></div>
+      <div className="flex-row goal-cards">
+        {UserGoals.map((goal, index) => {
+          return (
+            <div className="goal-card bg-white flex-column">
+              <div className="goal-card-top flex-column">
+                <span className="lato grey goal-card-amount">
+                  {goal.amount}
+                </span>
+                <span className="suprema faint grey goal-card-date">
+                  {goal.date}
+                </span>
+              </div>
+              <div className="goal-card-bottom flex-column">
+                <span className={`goal-card-icon goal-card-icon-${index + 1}`}>
+                  <i className={`${goal.icon}`}></i>
+                </span>
+                <span className="suprema primary goal-card-name">
+                  {goal.name}
+                </span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
