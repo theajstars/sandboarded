14c3a2336d486b7a64654ab186ea16d79a83bff4
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2022-03-07 21:21:02 +0100

    Get loan section

--- src/Components/NewTransaction.jsx
+++ src/Components/NewTransaction.jsx
@@ -18,10 +18,20 @@ export default function NewTransaction() {
               </div>
             );
           })}
-          <div className="add-transaction-person bg-yellow flex-row goals-plus primary pointer">
-            <i className="fas fa-plus"></i>
+          <div className="flex-column transaction-person add">
+            <span className="bg-yellow flex-row goals-plus primary pointer">
+              <i className="fas fa-plus"></i>
+            </span>
+            <span className="suprema transaction-person-name primary">
+              Add New
+            </span>
           </div>
         </div>
+      </div>
+      <div className="get-loan flex-row">
+        <span className="white suprema get-loan-header">
+          Get great <br /> loans
+        </span>
       </div>
     </div>
   );
