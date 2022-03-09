27e4bcf5c81454e5fb5f9f19d54b5d3065998490
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2022-03-09 13:10:16 +0100

    Began responsive design

--- src/Components/NewTransaction.jsx
+++ src/Components/NewTransaction.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { TransactionPeople } from "../Assets/JSON/TransactionPeople.json";
 export default function NewTransaction() {
+  const [amount, setAmount] = useState("");
+  // useEffect(() => {
+  //   setAmount(amount.toLocaleString())
+  // }, [amount])
   return (
     <div className="flex-row new-transaction-container">
       <div className="new-transaction-left bg-white">
@@ -26,6 +30,38 @@ export default function NewTransaction() {
               Add New
             </span>
           </div>
+        </div>
+        <br />
+        <div className="flex-row transfer-container">
+          <div className="flex-row new-transaction-amount-container bg-white">
+            <input
+              type="text"
+              className="suprema new-transaction-amount"
+              placeholder={0}
+              value={amount}
+              onChange={(e) => {
+                let value = e.target.value;
+                if (typeof value === Number) {
+                  // Amount is a number
+                  setAmount(value.toLocaleString());
+                } else if (value.length === 0) {
+                  //Amount is empty
+                } else {
+                  // Amount is a string
+                  let temp = value.split(",");
+                  temp = parseInt(temp.join(""));
+                  setAmount(temp.toLocaleString());
+                }
+              }}
+            />
+            <span className="lato new-transaction-amount-placeholder flex-row">
+              $
+            </span>
+          </div>
+          <span className="bg-yellow send-transfer primary quicksand flex-row pointer">
+            Send the transfer&nbsp;
+            <i className="far fa-angle-right" style={{ fontSize: "18px" }}></i>
+          </span>
         </div>
       </div>
       <div className="get-loan flex-row">
